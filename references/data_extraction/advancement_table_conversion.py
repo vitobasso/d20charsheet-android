2aeba6c536cddb665c54a7235a3c0253d176ed86
Victor Basso <vitobasso@gmail.com>
2015-04-21 00:39:00 -0300

    dndtools class advancement converter: class traits

--- references/data_extraction/advancement_table_conversion.py
+++ references/data_extraction/advancement_table_conversion.py
@@ -9,7 +9,7 @@ cleanRegex = r'_\\.*|^"? *\| *| *\| *"?$| *-- *(?=\|)|_\. *|(?<=\d)st (?=\|)|(?<
 
 babMap = [None, 'POOR', 'AVERAGE', 'GOOD']
 resistMap = ['POOR', None, 'GOOD']
-
+traitCount = 0
 
 class ClassAdv:
     def __init__(self):
@@ -32,72 +32,107 @@ def cleanTable(tableStr):
 
 def readAdvancementRow(row, classAdv):
     level = int(row['Level'])
-    if (level == 1):
+    if level == 1:
         classAdv.fort = resistMap[int(row['Fort'])]
         classAdv.refl = resistMap[int(row['Ref'])]
         classAdv.will = resistMap[int(row['Will'])]
-    if (level == 3):
+    if level == 3:
         classAdv.bab = babMap[int(row['BAB'])]
-    if('Special' in row):
+    if 'Special' in row:
         traitStr = row['Special']
         traitList = re.split(r', *', traitStr)
         classAdv.traits[level - 1] = traitList
 
 
 def readAdvanementTable(tableLines):
-    classAdv = ClassAdv()
+    advModel = ClassAdv()
     reader = csv.DictReader(tableLines, delimiter='|', quoting=csv.QUOTE_NONE)
     failCount = 0
     for row in reader:
         try:
-            readAdvancementRow(row, classAdv)
+            readAdvancementRow(row, advModel)
         except Exception as e:
             failCount += 1
             #print('Bad row: ', row)
 
-    return classAdv, failCount
+    return advModel, failCount
 
 
 def readRow(row):
     advStr = row['advancement']
     advLines = cleanTable(advStr)
     result, fails = readAdvanementTable(advLines)
-    if (fails > 1):
+    if fails > 1:
         print(row['id'], row['name'], 'fails:', fails)
     return result, fails
 
 
-def convertHeaders(oldHeaders):
+def createClassHeaders(oldHeaders):
     newHeaders = oldHeaders[:]
     pos = newHeaders.index('advancement')
     newHeaders.remove('advancement')
-    newHeaders[pos:pos] = ['bab', 'fort', 'refl', 'will', 'traits']
+    newHeaders[pos:pos] = ['bab', 'fort', 'refl', 'will']
     return newHeaders
 
 
-def convertRow(row, failCount):
-    adv, rowFails = readRow(row)
-    if (rowFails > 1):
-        failCount['manyLines'] += 1
-    elif (rowFails > 0):
-        failCount['oneLine'] += 1
-    newRow = dict(row, bab=adv.bab, fort=adv.fort, refl=adv.refl, will=adv.will, traits=adv.traits)
+def createClassWriter(outf, oldHeaders):
+    newHeaders = createClassHeaders(oldHeaders)
+    writer = csv.DictWriter(outf, newHeaders, delimiter=';', quotechar='"', lineterminator='\n')
+    writer.writeheader()
+    return writer
+
+
+def writeClassRow(writer, row, model):
+    newRow = dict(row, bab=model.bab, fort=model.fort, refl=model.refl, will=model.will)
     newRow.pop('advancement')
-    return newRow
+    writer.writerow(newRow)
 
 
-def convertCsv(inFile, outFile):
-    with open(inFile, 'r') as inf, open(outFile, 'w') as outf:
+def createTraitsWriter(outf):
+    headers = ['id', 'rulebook_id', 'page', 'class_id', 'level', 'name']
+    writer = csv.DictWriter(outf, headers, delimiter=';', quotechar='"', lineterminator='\n')
+    writer.writeheader()
+    return writer
+
+
+def writeTraitRow(writer, row, level, name):
+    global traitCount
+    traitCount += 1
+    row = dict(id=traitCount, rulebook_id=row['rulebook_id'], page=row['page'], class_id=row['id'], level=level, name=name)
+    writer.writerow(row)
+
+
+def writeClassTraitsForRow(writer, row, model):
+    for i, traitList in enumerate(model.traits):
+        level = i + 1
+        if not traitList:
+            continue
+        for trait in traitList:
+            if not trait:
+                continue
+            writeTraitRow(writer, row, level, trait)
+
+
+def countFails(failCount, rowFails):
+    if rowFails > 1:
+        failCount['manyLines'] += 1
+    elif rowFails > 0:
+        failCount['oneLine'] += 1
+
+
+def convertCsv(inFile, classFile, traitFile):
+    with open(inFile, 'r') as inf, open(classFile, 'w') as classf, open(traitFile, 'w') as traitf:
         reader = csv.DictReader(inf, delimiter=';', quotechar='"')
-        newHeaders = convertHeaders(reader.fieldnames)
-        writer = csv.DictWriter(outf, newHeaders, delimiter=';', quotechar='"', lineterminator='\n')
-        writer.writeheader()
+        classWriter = createClassWriter(classf, reader.fieldnames)
+        traitsWriter = createTraitsWriter(traitf)
         failCount = dict(oneLine=0, manyLines=0)
         for row in reader:
-            newRow = convertRow(row, failCount)
-            writer.writerow(newRow)
+            model, rowFails = readRow(row)
+            countFails(failCount, rowFails)
+            writeClassRow(classWriter, row, model)
+            writeClassTraitsForRow(traitsWriter, row, model)
         print('failed one line:', failCount['oneLine'], 'many lines:', failCount['manyLines'])
 
 
 dir = '../../app/src/main/assets/data/csv/'
-convertCsv(dir + 'classes.csv', dir + 'classes_new.csv')
+convertCsv(dir + 'classes.csv', dir + 'classes_new.csv', dir + 'classes_trait.csv')
